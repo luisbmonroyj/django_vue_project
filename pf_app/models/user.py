0397d588eb8bdb74677b8bc9d7ec8c4cc5a92157
santiago.mlondono4 <santiago.mlondono4@gmail.com>
2022-07-27 17:31:32 -0500

    Creación del modelo de usuarios

---
+++ pf_app/models/user.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+
+        if not username:
+            raise ValueError('Users must have an username')
+        elif not password:
+            raise ValueError('Users must have a password')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, username, password):
+
+        user = self.create_user(username=username, password=password,)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField('Username', max_length = 15, unique=True)
+    password = models.CharField('Password', max_length = 256)
+    name = models.CharField('Name', max_length = 30)
+    email = models.EmailField('Email', max_length = 100)
+    def save(self, **kwargs):
+        some_salt = '5tgT5678Ikju89IKj'
+        self.password = make_password(self.password, some_salt)
+        AbstractBaseUser.save(**kwargs)
+        #super().save(**kwargs)
+        objects = UserManager()
+        USERNAME_FIELD = 'username'
