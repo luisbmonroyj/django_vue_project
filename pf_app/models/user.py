94807b38b683e545fe917296a783ddff307b89ed
luisbmonroyj <lbmonroyj@gmail.com>
2022-07-31 18:21:47 -0500

    de vuelta al main

--- pf_app/models/user.py
+++ pf_app/models/user.py
@@ -21,11 +21,19 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.BigAutoField(primary_key=True)
-    username = models.CharField('Username', max_length = 15, unique=True)
-    password = models.CharField('Password', max_length = 256)
-    name = models.CharField('Name', max_length = 30)
-    email = models.EmailField('Email', max_length = 100)
+    #papasFersanuser no tiene id
+    #usuario_id = models.CharField(primary_key=True)
+    #username de ppasfersan es email
+    id_usuario = models.BigAutoField('id_usuario',primary_key=True)
+    username = models.CharField('username',max_length=30, unique=True ,default='loser')
+    password = models.CharField('password',max_length=300)
+    apellido = models.CharField('apellido',max_length=30,default='apellido')
+    nombre = models.CharField('nombre',max_length=30,default='nombre')
+    email = models.EmailField('correo',max_length=100,default='nombre@dominio.com')
+    direccion = models.CharField('direccion',max_length=50,default='calle 1 carrera 1 # 1')
+    telefono = models.CharField('telefono',max_length=15,default='00000000')
+    estado = models.BooleanField('activo',default=True)
+
     def save(self, **kwargs):
         some_salt = '5tgT5678Ikju89IKj'
         self.password = make_password(self.password, some_salt)
