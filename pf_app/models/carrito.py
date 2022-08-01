94ab9a8013855c64e9f02c572a59f12b7fe74280
luisbmonroyj <lbmonroyj@gmail.com>
2022-07-31 20:07:19 -0500

    hasta aqui funciona

--- pf_app/models/carrito.py
+++ pf_app/models/carrito.py
@@ -1,14 +1,13 @@
 from django.db import models
-from pf_app.models import Producto
-from pf_app.models import User
+from pf_app.models import producto
+from pf_app.models import user
 
 class Carrito(models.Model):
     #id_carrito = models.BigAutoField('id_carrito', primary_key= True)
-    id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    #productos = models.ForeignKey(producto.Producto, on_delete=models.CASCADE)
-    productos = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(user.User, on_delete=models.CASCADE)
+    productos = models.ForeignKey(producto.Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField('cantidad', default= 1)
-    costo = models.FloatField(Producto.precio,default=0)
+    #costo = models.FloatField(Producto.precio,default=0)
     #valor = models.FloatField(costo*cantidad ,default=0)
     """
     def __str__(self):
