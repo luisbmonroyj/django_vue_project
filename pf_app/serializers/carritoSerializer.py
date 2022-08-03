2a72061194b6cd6b2e277847710a4ac324c4c6d4
luisbmonroyj <lbmonroyj@gmail.com>
2022-08-03 13:23:24 -0500

    SANTIAGO leer Comentarios en carritoSerializer.

--- pf_app/serializers/carritoSerializer.py
+++ pf_app/serializers/carritoSerializer.py
@@ -8,7 +8,7 @@ class CarritoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carrito
         fields = '__all__'
-        #['id_usuario','password','apellido','nombre','telefono','direccion','estado']
+        #['id_carrito','id_usuario','productos','cantidad','costo','id_pedido']
 
     def create(self,validated_data):
         carritoInstance = Carrito.objects.create(**validated_data)
@@ -18,17 +18,19 @@ class CarritoSerializer(serializers.ModelSerializer):
         #carrito = Carrito.objects.all()
         carrito = Carrito()
         usuario = User()
-        Producto.objects.get(id_producto=1).carrito_se.all()
-        Producto.objects.all().filter(carrito__id_usuario=1)
-        carrito.objects.create(id_usuario=1,cantidad=5
-        productos=Producto.objects.get(id_producto=1))
+        #Producto.objects.get(id_producto=1).carrito_se.all()
+        #Producto.objects.all().filter(carrito__id_usuario=1)
+        #carrito.objects.create(id_usuario=1,cantidad=5)
+        #productos=Producto.objects.get(id_producto=1))
         Carrito.objects.filter(producto__id_producto=1)
         return {
             'id_usuario': usuario.id_usuario,
-                'productos': carrito.productos,
+                #podriamos usar Carrito.id_usuario? Porque ese dato esta guardado en el META de carrito
+                'productos': carrito.productos, 
+                #podriamos usar el diccionario que estamos usando en Pedido? el que relaciona id_producto:cantidad?
                 'cantidad': carrito.cantidad,
                 #'costo': producto.objects.get(id_producto = carrito.productos)
                 }
-            ##    'estado': user.estado
+            ##   'estado': user.estado
             #    considero que estado no deberia mandarlo, dejarlo como tratamiento interno
             
