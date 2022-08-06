9bd45e8c9fa8df27f8848c860a82762057b4bc00
santiago.mlondono4 <santiago.mlondono4@gmail.com>
2022-08-06 12:08:08 -0500

    Monitoria del sabado

--- pf_app/views/cartView.py
+++ pf_app/views/cartView.py
@@ -1,49 +1,66 @@
+from posixpath import split
+from tkinter import CENTER
 from rest_framework import status,views
 from rest_framework.response import Response
 #from pf_app.models.carrito import Carrito
 from pf_app.serializers.pedidoSerializer import PedidoSerializer
 from pf_app.serializers.carritoSerializer import CarritoSerializer
+from pf_app.models.producto import Producto
 #from pf_app.serializers.productoSerializer import ProductoSerializer
 
 class CarritoCreateView(views.APIView):
     def post(self, request, *args, **kwargs):
-        serializer = CarritoSerializer(data = request.data) #llama al metodo create de carritoSerializer
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        #serializer = CarritoSerializer(data = request.data) #llama al metodo create de carritoSerializer
+        #serializer.is_valid(raise_exception=True)
+        #serializer.save()
         
-        carritoData = {
-            'id_usuario':request.data['id_usuario'],
-            'productos_usuario':{'productos':request.data['productos'],'cantidad':request.data['cantidad'],}
-            #'costo':request.data['costo'],
-            }
-            #un ejemplo de carrito es asi
-            #carritoData = {'id_usuario':1,
-            #'productos_usuario':{'1': 4, '2': 8, '3': 12, '4': 16}
-            #}
+        # carritoData = {
+        #     'id_usuario':request.data['id_usuario'],
+        #     'productos_usuario':{'productos':request.data['productos'],'cantidad':request.data['cantidad'],}
+        #     #'costo':request.data['costo'],
+        #     }
 
+        
         #posiblemente no lo necesite
-        userid = carritoData['id_usuario']
+        userid = request.data['id_usuario']
         #lista debe tener las llaves y los valores del segundo elemento de del diccionario carritoData
-        lista = carritoData['productos_usuario']#segun el ejemplo, esta linea es
+        lista = request.data['productos_usuario']#segun el ejemplo, esta linea es
         #{'1': 4, '2': 8, '3': 12, '4': 16}
-        cantidades = list(lista.values())#segun el ejemplo, esta linea es
+        #cantidades = lista.values()#segun el ejemplo, esta linea es
         #[4,8,12,16]
-        productos_diccionario = list(lista.keys())#segun el ejemplo, esta linea es
+        #productos_diccionario = lista.keys()#segun el ejemplo, esta linea es
         #['1','2','3','4'] que son los id_producto que escogio el cliente
         #NECESITO UNA LISTA QUE ME TRAIGA LOS PRECIOS DE LA TABLA PRODUCTO
         #para calcular el costo(=cantidad*Producto.precio) y sumarlos para hallar total
         #lista = carritoData['productos_usuario'] 
         total = 0
-        cantidades = lista.values()
-        productos_diccionario = lista.keys()
+        #cantidades = lista.values()
+        #productos_diccionario = lista.keys()
         #bota una clase diccionario
         costos = {}
-        for i in productos_diccionario:
-            costo = 0
-            productoDB = request.filter(id_producto=productos_diccionario[i]).first
-            costo = productoDB.precio * request.lista(cantidades[i])
-            costos[i] = costo
-            total += costo   #### Acá se obtiene el total TOTAL
+        variable_luis = Producto.objects.all()
+        print(variable_luis)
+        print('llegué')
+        print(lista)
+        #print(productos_diccionario)
+        h = 0
+        for i in lista:
+            # i -> diccionario
+            j = i.values()
+            k = i.keys()
+            print((j))
+            print((k))
+            for z, l in i.items():
+                costo = 0
+                productoDB = variable_luis.get(pk=int(z))
+                costo = productoDB.precio * l
+                costos[h] = costo
+                h += 1
+                total += costo   #### Acá se obtiene el total TOTAL
+        
+        print(total)
+        print(costos)
+        
         
         diccionario_entrega = {
             'total':total,
@@ -54,20 +71,20 @@ class CarritoCreateView(views.APIView):
 
         # for i in productos_diccionario:
         #     texto = '{\n' + f'"id_usuario":{userid}, \n"productos_usuario": {productos_diccionario[int(i)-1]}, \n"cantidad": {cantidades[int(i)-1]}' + '\n}'
-        #     serializer.create(texto)        
+        #     serializer.create(texto)
 
         for i in productos_diccionario: #productos_diccionario son las llaves de lista, y lista es el diccionario {id_producto:cantidad}
-            y = {
+            carritoData = {
                 'id_pedido':pedidoCompleto.id_pedido,
                 'id_usuario':pedidoCompleto.id_usuario,
                 'productos': productos_diccionario[i],
                 'cantidad':cantidades[i],
                 'costo':costos[i]
             }
-            return Response(y,status = status.HTTP_201_CREATED)
+            return Response(carritoData,status = status.HTTP_201_CREATED)
 
-        for i in range (len(productos)):
-            costo = 0
-            productoDB = request.filter(id_producto=productos[i]).first
-            costo = productoDB.precio * request.Productos_Usuarios.get(productos[i])	
-            carrito.save	(request.id_usuario,id_producto,cantidad,costo,pedidoCompleto.id)
+        # for i in range (len(productos)):
+        #     costo = 0
+        #     productoDB = request.filter(id_producto=productos[i]).first
+        #     costo = productoDB.precio * request.Productos_Usuarios.get(productos[i])	
+        #     carrito.save	(request.id_usuario,id_producto,cantidad,costo,pedidoCompleto.id)
