c605a4960e5dcb98cb64bc2b80634f513699e509
julianzoque <julianzoque@gmail.com>
2022-08-07 16:44:30 -0500

    solucion errores y creacion botones en header

--- pf_fe/src/App.vue
+++ pf_fe/src/App.vue
@@ -2,8 +2,29 @@
   <div id="app" class="app">
 
     <div class="header">
+
       <h1>Papas fersan</h1>
+      <nav>
+        <button v-if="is_auth" > Inicio </button>
+        <button v-if="is_auth" > Cuenta </button>
+        <button v-if="is_auth" > Cerrar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadSignUp" > Registrarse </button>
+      </nav>
     </div>
+
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+      >
+      </router-view>
+    </div>
+
+    <div class="footer">
+      <h2>Papas Fersan misión tic 2022</h2>
+    </div>
+
   </div>
 </template>
 
